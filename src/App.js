04091817e109aee65e2962c850fe21bcc95bea80
Stéphane Chimy <stephane.akira@gmail.com>
2021-06-14 16:06:46 +0200

    Creation du textArea + modifications dans le textArea + render avec marked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
+import { sampleText } from "./sampleText"
+import marked from "marked"
+import DOMPurify from "dompurify"
 
-function App() {
+const App = () => {
+  let [text, setText] = useState(sampleText)
+
+  // Changement d'état dans le textArea
+  const handleChange = (event) => {
+    const newText = event.target.value
+    setText((text = newText))
+  }
+
+  // Fonction retournant le text compilé par marked
+  const renderText = (text) => {
+    let __html
+    __html = marked(DOMPurify.sanitize(text))
+    return { __html }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <textarea className='form-control' value={text} rows='35' onChange={handleChange}></textarea>
+        </div>
+
+        <div className='col-sm-6'>
+          <div dangerouslySetInnerHTML={renderText(text)}></div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
